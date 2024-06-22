8804016969ec05bd6e0377bb02c1a1942629c234
Yuefeng Geng <maxwellgeng@outlook.com>
2024-06-22 11:14:35 +0800

    Update anyhash.h

--- include/daScript/misc/anyhash.h
+++ include/daScript/misc/anyhash.h
@@ -22,26 +22,11 @@ namespace das {
     }
 
     static NO_ASAN_INLINE uint64_t hash_blockz64 ( const uint8_t * block ) {
-        auto FNV_offset_basis = 14695981039346656037ul;
-        auto FNV_prime = 1099511628211ul;
-        if ( !block ) return FNV_offset_basis;
-        auto h = FNV_offset_basis;
-#if DAS_SAFE_HASH
-        while ( *block ) {
-            h ^= *block++;
-            h *= FNV_prime;
+        auto end_block = block;
+        while ( *end_block ) {
+            end_block++;
         }
-#else
-        while ( true ) {
-            uint64_t v = *(uint16_t *)block;
-            if ( (v & 0xff)==0 ) break;
-            h ^= v;
-            h *= FNV_prime;
-            if ( v < 0x100 ) break;
-            block += 2;
-        }
-#endif
-        return h <= HASH_KILLED64 ? 1099511628211ul : h;
+        return hash_block64(block, end_block - block);
     }
 
     static __forceinline uint64_t hash_uint32 ( uint32_t value ) {  // this is simplified, and not the same as wyhash(&value,4)
